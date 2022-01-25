ed4bdb2c84e99501dacb3e59ef3f35ba367a59cd
Prasanna_bala <bprasanna83@gmail.com>
2022-01-25 10:23:28 +0100

    first commit backend

---
+++ shareme_backend/schemas/user.js
@@ -0,0 +1,18 @@
+/*user scheme*/
+export default{
+    name:'user',
+    title:'user',
+    type:'document',
+    fields:[
+        {
+            name:'userName',
+            title:'UserName',
+            type:'string',
+        } ,
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string',
+        },
+    ],
+};
