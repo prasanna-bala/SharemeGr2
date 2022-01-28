fcb71c4359fdf3585e76d7013cb341a8c211c486
mocquaischristopher <73651871+mocquaischristopher@users.noreply.github.com>
2022-01-28 12:06:04 +0100

    add spinner and update data.js

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,24 +1,37 @@
-import React, {
-    useState,
-    useEffect
-} from 'react';
-import {
-    useParams
-} from 'react-router-dom';
-import {
-    client
-} from '../client';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { client } from '../client';
+import { searchQuery } from '../utils/data';
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
 
 const Feed = () => {
+        const [loading, setLoading] = useState(false);
+        const [pins, setPins] = useState(null);
+        const {catogoryId} = useParams();
 
-        const [loading, setLoading] = useState(true);
+        useEffect(() => {
+            setLoading(true);
+            if(categoryId) {
+                const query = searchQuery(categoryId);
+                client.fetch(query)
+                    .then((data) => {
+                        setPins(data);
+                        setLoading(false);
+                    });
+            } else {
+
+            }
+        }, [categoryId])
 
         if (loading) return <Spinner message = 'we are adding new ideas  to your feed ! ' /> ;
             
-        return( <div> Feed </div>);
-        };
+        return( 
+            <div> 
+                Feed 
+            </div>
+        );
+};
 
         export default Feed;
