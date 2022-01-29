dba3d8d12ae773ec141133c07fd8d56e876232bf
Prasanna_bala <bprasanna83@gmail.com>
2022-01-29 16:14:18 +0100

    create pin

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -4,12 +4,40 @@ import { MdDelete } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
 import { client } from '../client';
 import Spinner from './Spinner';
+import { categories } from '../utils/data';
 
 // categories  [{name: 'sports' ,image:'' }]
 
 const CreatePin = ({ user }) => {
 
-  return <div>create pin</div>;
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [destination, setDestination] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [fields, setFields] = useState(false);
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null); 
+  const [wrongImageType, setWrongImageType] = useState(false);
+  const navigate = useNavigate();
+
+  return(
+   
+    <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
+      {fields && (
+        <p className='text-red-500 mb-5 text-xl transition-all duration-150 ease-in'>
+          Please fill in all the fields.
+        </p>
+      )}
+      <div className='flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full'>
+        <div className='bg-secondaryColor p-3 flex flex-0.7 w-full'>
+          <div className='flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420'>
+            {loading && <Spinner />}
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
 };
 
 export default CreatePin;
