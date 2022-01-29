6fc3fc638282747e98f9eefb3dada8e5bfc958b3
Prasanna_bala <bprasanna83@gmail.com>
2022-01-29 17:25:39 +0100

    create pin and add the userprofile details in createpin

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -13,13 +13,33 @@ const CreatePin = ({ user }) => {
   const [title, setTitle] = useState('');
   const [about, setAbout] = useState('');
   const [destination, setDestination] = useState('');
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [fields, setFields] = useState(false);
   const [category, setCategory] = useState(null);
   const [imageAsset, setImageAsset] = useState(null); 
   const [wrongImageType, setWrongImageType] = useState(false);
   const navigate = useNavigate();
 
+  const uploadImage = (e) => {
+    const selectedFile = e.target.files[0];
+    if(selectedFile.type === 'image/png' ||  selectedFile.type === 'image/svg' || selectedFile.type === 'image/jpeg' ||selectedFile.type === 'image/gif' || selectedFile.type === 'image/tiff' ) {
+      setWrongImageType(false);
+      setLoading(true);
+      client.assets
+        .upload('image',selectedFile, {contentType: selectedFile.type, fileName: selectedFile.name})
+        .then((document) => { 
+          setImageAsset(document);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log ('Image upload error ', error );
+        })
+    }else
+    {
+      setLoading(false); 
+      setWrongImageType(true);
+    }
+  }
   return(
    
     <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
@@ -32,7 +52,61 @@ const CreatePin = ({ user }) => {
         <div className='bg-secondaryColor p-3 flex flex-0.7 w-full'>
           <div className='flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420'>
             {loading && <Spinner />}
+            {wrongImageType && <p> Wrong image type</p>}
+            {!imageAsset ? (
+              <label>
+                <div className='flex flex-col items-center justify-center h-full'>
+                  <div  className='flex flex-col justify-center items-center'>
+                    <p className= 'font-bold text-2xl '>
+                      <AiOutlineCloudUpload />
+                    </p>
+                    <p className= 'text-lg'>
+                      Click to upload.
+                    </p>
+                  </div>
+                  <p className= 'mt-32 text-gray-400'>
+                    Use high-quality JPG, PNG, SVG, GIF less than 20MB.
+                  </p>
+                </div>
+                <input 
+                  type="file"
+                  name="upload-image"
+                  onChange={uploadImage}
+                  className="w-0 h-0"
+                />
+              </label>
+            ) : (
+                  <div className="realative h-full">
+                    <img src={ imageAsset?.url } alt="uploaded-pic" className="h-full w-full"/>
+                    <button type="button" 
+                    className="absolute bottom-3 right-3 p-3 rounded-full bg-white text-xl cursor-pointer outline-none hover:shadow-md transition-all duration-500 ease-in-out"
+                    onClick={() => setImageAsset(null)}
+                    >
+                      <MdDelete />
+                    </button>
+                  </div>
+            )}
           </div>
+        </div>
+        <div className="flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full">
+          <input 
+          type = "text"
+          value ={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder = 'Add your title here'
+          className="outline-none text-2xl sm:text-3xl font-bold border-b-2 border-gray-200  p-2"
+          />
+          {
+            user && (
+              <div className="flex gap-2 my-2 items-center bg-white rounded-lg">
+                <img src={user.image}
+                  className="w-10 h-10 rounded-full"
+                  alt = 'user-profile' 
+                />
+                <p className="font-bold">{user.userName} </p>
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
