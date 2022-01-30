9e8ec04fcb0cd72b9d719b223439ff3a480c4814
Prasanna_bala <bprasanna83@gmail.com>
2022-01-30 12:32:44 +0100

    component create pindetails error

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { AiOutlineCloudUpload } from 'react-icons/ai';
 import { MdDelete } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
@@ -12,12 +12,13 @@ const CreatePin = ({ user }) => {
 
   const [title, setTitle] = useState('');
   const [about, setAbout] = useState('');
-  const [destination, setDestination] = useState('');
+  const [destination, setDestination] = useState();
   const [loading, setLoading] = useState(false);
-  const [fields, setFields] = useState(false);
-  const [category, setCategory] = useState(null);
-  const [imageAsset, setImageAsset] = useState(null); 
+  const [fields, setFields] = useState();
+  const [category, setCategory] = useState();
+  const [imageAsset, setImageAsset] = useState(); 
   const [wrongImageType, setWrongImageType] = useState(false);
+
   const navigate = useNavigate();
 
   const uploadImage = (e) => {
@@ -32,7 +33,7 @@ const CreatePin = ({ user }) => {
           setLoading(false);
         })
         .catch((error) => {
-          console.log ('Image upload error ', error );
+          console.log ('Upload Failed: ', error.message );
         })
     }else
     {
@@ -54,24 +55,26 @@ const CreatePin = ({ user }) => {
             _ref: imageAsset?._id
           }
         },
-        userId:user._id,
-        postedBy:{
-          _type:'postedBy',
+        userId: user._id,
+        postedBy: {
+          _type: 'postedBy',
           _ref: user._id,
         },
         category,
       }
       client.create(doc)
       .then(() =>{
-        navigate('/')
-      })
+        navigate('/');
+      });
     } else {
       setFields(true);
-      setTimeout(() => setFields(false), 2000)
+      setTimeout(() => { setFields(false);
+    }, 2000, );
     }
-  }
+  };
+
   return(
-   
+  
     <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
       {fields && (
         <p className='text-red-500 mb-5 text-xl transition-all duration-150 ease-in'>
@@ -81,8 +84,10 @@ const CreatePin = ({ user }) => {
       <div className='flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full'>
         <div className='bg-secondaryColor p-3 flex flex-0.7 w-full'>
           <div className='flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420'>
-            {loading && <Spinner />}
-            {wrongImageType && <p> Wrong image type</p>}
+            {loading && (
+              <Spinner />)
+              }
+            {wrongImageType && ( <p>It&apos;s Wrong image type</p>)}
             {!imageAsset ? (
               <label>
                 <div className='flex flex-col items-center justify-center h-full'>
@@ -95,7 +100,7 @@ const CreatePin = ({ user }) => {
                     </p>
                   </div>
                   <p className= 'mt-32 text-gray-400'>
-                    Use high-quality JPG, PNG, SVG, GIF less than 20MB.
+                 Recommendation:  Use high-quality JPG, PNG, SVG, GIF less than 20MB.
                   </p>
                 </div>
                 <input 
@@ -145,7 +150,7 @@ const CreatePin = ({ user }) => {
           className="outline-none text-base sm:text-lg border-b-2 border-gray-200  p-2"
           />
           <input 
-          type = "text"
+          type = "url"
           value ={destination}
           onChange={(e) => setDestination(e.target.value)}
           placeholder = 'Add destination link'
@@ -156,11 +161,11 @@ const CreatePin = ({ user }) => {
               <p className='mb-2 font-semibold sm:text-xl text-lg'>
                 choose pin category
               </p>
-              <select onChange={(e) => setCategory(e.target.value)}
+              <select onChange={(e) => { setCategory(e.target.value);}}
                 className="outline-none w-4/5 text-base border-b-2 border-gray-200 p⁻2 rounded-md cursor-pointer">
                 <option value="other" className="bg-white">Select Category</option>
-                {categories.map((category) =>(
-                  <option className="text-base border-0 outline-none capitalize bg-white text-black" value={category.name}> {category.name}</option>
+                {categories.map((item) =>(
+                  <option className="text-base border-0 outline-none capitalize bg-white text-black" value={item.name}> {item.name}</option>
                 ))}
               </select>
             </div>
