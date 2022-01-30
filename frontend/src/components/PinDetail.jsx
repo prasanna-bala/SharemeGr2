49a710bda95d99a126fa8d9f99168cb73352c05c
Prasanna_bala <bprasanna83@gmail.com>
2022-01-30 11:50:36 +0100

    component create pindetails add comments and saves

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -36,6 +36,27 @@ const PinDetail = ({user}) => {
     fetchPinDetails();
   },[pinId])
 
+  const addComment =()=>{
+    if (comment)
+    { 
+      setAddingComment(true);
+
+      client
+        .patch(pinId)
+        .setIfMissing({ comments :[]})
+        .insert('after','comments[-1]',[{comment, _key: uuidv4(), postedBy: {
+          _type:'postedBy',
+          _ref: user._id
+        }}])
+        .commit()
+        .then(() => {
+          fetchPinDetails();
+          setComment('');
+          setAddingComment(false);
+        });
+    }
+  };
+
   if(!pinDetail) return <Spinner message="Loading Pin..."/>
   
   return( 
@@ -45,7 +66,7 @@ const PinDetail = ({user}) => {
         className="rounded-t-3xl rounded-b-lg" />
       </div>
       <div className="w-full p-5 flex-1 xl:min-w-620">
-        <did className='flex items-center justify-between'>
+        <div className='flex items-center justify-between'>
           <div className="flex gap-2 items-center">
             <a
               href={`${pinDetail.image.asset.url}?dl=`}
@@ -58,7 +79,51 @@ const PinDetail = ({user}) => {
           <a href= {pinDetail.destination} target='_blank' rel="noreferrer">
             { pinDetail.destination }
           </a>
-        </did>
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold break-words mt-3"> 
+            { pinDetail.title }
+          </h1>
+          <p className="mt-3">
+            { pinDetail.about }
+          </p>
+        </div>
+        <Link to={`/user-profile/${pinDetail?.postedBy._id}`} className="flex gap-2 mt-5 items-center bg-white rounded-lg ">
+          <img src={pinDetail?.postedBy.image} className="w-10 h-10 rounded-full" alt="user-profile" />
+          <p className="font-bold">{pinDetail?.postedBy.userName}</p>
+        </Link>
+        <h2 className="mt-5 text-2xl"> Comments</h2>
+        <div className="max-h-370 overflow-y-auto" >
+          { pinDetail?.comments?.map((item) =>(
+            <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={item.comment}> 
+              <img src={item.postedBy?.image}
+                alt='user-profile'
+                className="w-10 h-10 rounded-full cursor-pointer ">
+
+              </img>
+              <div className="flex flex-col">
+                <p className="font-bold">{ item.postedBy?.userName}</p>
+                <p>{ item.comment }</p>
+                
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-wrap mt-6 gap-3">
+        <Link to={`/user-profile/${user._id}`} >
+          <img src={user.image} className="w-10 h-10 rounded-full cursor-pointer" alt="user-profile" />
+        </Link>
+        <input className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300" 
+          type="text" 
+          placeholder= 'Add a Comment' 
+          value = { comment }
+          onChange={(e)=> setComment(e.target.value)}>
+        </input>
+        <button type="button" className='bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none'
+          onClick={addComment}>
+          { addingComment ? 'Doing...': 'Done'}
+        </button>
+        </div>
       </div>
     </div>
   );
